0a09391e9bbc50152800bba755c562ebea4b0d15
Brian Stearns <brianstearns@Brians-MacBook-Pro-2.local>
2022-08-04 14:57:02 -0400

    Try again.

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,14 @@ fn pong(mut stream: &TcpStream) {
 
 fn main() {
 
-    let aws_metadata = "251.254.169.254";
-    let aws_metadata_port = 80;
+    // let aws_metadata = "251.254.169.254";
+    // let aws_metadata_port = 80;
     
 
     // Check for aws metadata
-    let resp = reqwest::blocking::get(&format!("http://{}:{}/latest/meta-data/public-ipv4", aws_metadata, aws_metadata_port)).unwrap();
+    // let resp = reqwest::blocking::get(&format!("http://{}:{}/latest/meta-data/public-ipv4", aws_metadata, aws_metadata_port)).unwrap();
     // let resp = reqwest::blocking::get(&format!("http://google.com")).unwrap();
-    println!("{}", resp.text().unwrap());
+    // println!("{}", resp.text().unwrap());
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
